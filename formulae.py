af575ec0eddefff0b7d19be4f17475d971a5a755
beingsage <156518249+beingsage@users.noreply.github.com>
2025-09-06 21:20:51 +0530

    Update formulae.py

--- formulae.py
+++ formulae.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 def calculate_indices(df):
-    df["Qi"] = ((df["Ci"] - df["Ii"]) / (df["Si"] - df["Ii"])) * 100
+    df["Qi"] = ((df["Ci"] - df["Ii"]) / (df["Si"] - df["Ii"])) * 100  
     df["Wi"] = (1 / df["Si"]) / (1 / df["Si"]).sum()
     df["Cfi"] = df["Ci"] / df["Si"]
 
@@ -13,3 +13,14 @@ def calculate_indices(df):
     return HPI, HEI, Cd
 
 
+
+
+// all heavy metal(45, 60), thier organic, chemical, pyhsical reqctions as same as real world (1k+) 
+// mole of every reaction, chained reactions
+// as -> aso2 -> pbo2 => aso2 pb02 > phyiccal, impllications 
+// csv excel 
+// csv -> json -> json -> csv 
+// routes expose -> microservice archietecutre
+// impacts and thier description of equations 
+// humidity, turbidty, temperature 
+    // date ->  
